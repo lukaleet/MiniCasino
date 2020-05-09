cf132d5b15dcdf2f5a3e2f47fd512a55019895a1
luke <2lukaszmaron@gmail.com>
2020-05-09 18:06:15 +0200

    Test Commit 2

--- WPFUI/ViewModels/ShellViewModel.cs
+++ WPFUI/ViewModels/ShellViewModel.cs
@@ -10,71 +10,19 @@ namespace WPFUI.ViewModels
 {
 	public class ShellViewModel : Conductor<object>
 	{
-		private string _firstName;
-		private string _lastName;
-		private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
-		private PersonModel _selectedPerson;
+		//private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
+		private SlotsViewModel _slotsVM;
+		private 
 
-		public ShellViewModel()
+
+		public ShellViewModel(SlotsViewModel slotsVM)
 		{
-			People.Add(new PersonModel { FirstName = "Luka", LastName = "Maron" });
-			People.Add(new PersonModel { FirstName = "UgaBuga", LastName = "Bbbbb" });
-			People.Add(new PersonModel { FirstName = "Aye", LastName = "Aye" });
+			_slotsVM = slotsVM;
 		}
 
-		public string FirstName
+		public void SlotsButton()
 		{
-			get { return _firstName; }
-			set
-			{
-				_firstName = value;
-				NotifyOfPropertyChange(() => FirstName);
-				NotifyOfPropertyChange(() => FullName);
-			}
-		}
 
-		public string LastName
-		{
-			get { return _lastName; }
-			set
-			{
-				_lastName = value;
-				NotifyOfPropertyChange(() => LastName);
-				NotifyOfPropertyChange(() => FullName);
-			}
-		}
-
-		public string FullName
-		{
-			get { return $"{FirstName} {LastName}"; }
-
-		}
-
-		public BindableCollection<PersonModel> People
-		{
-			get { return _people; }
-			set { _people = value; }
-		}
-
-		public PersonModel SelectedPerson
-		{
-			get { return _selectedPerson; }
-			set
-			{
-				_selectedPerson = value;
-				NotifyOfPropertyChange(() => SelectedPerson);
-			}
-		}
-
-		public bool CanClearText(string firstName, string lastName)
-		{
-			return !String.IsNullOrWhiteSpace(firstName) || !String.IsNullOrWhiteSpace(lastName);
-		}
-
-		public void ClearText(string firstName, string lastName) 
-		{
-			FirstName = "";
-			LastName = "";
 		}
 	}
 }
