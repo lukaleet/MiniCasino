bffc4c6886851722dad7f25caedc4b6b03221c94
luke <2lukaszmaron@gmail.com>
2020-05-12 21:01:55 +0200

    Add Simple Slots Logic, Slots Are Officialy In Beta 1.0 Added simple slots logic check (3 of a kind), images and slots are changing each play now. Lets do this boy.
    Note: validation was added in earlier commit

--- WPFUI/ViewModels/SlotsViewModel.cs
+++ WPFUI/ViewModels/SlotsViewModel.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
 
 namespace WPFUI.ViewModels
 {
@@ -17,6 +19,10 @@ namespace WPFUI.ViewModels
 		private string _betInfo;
 		// tymczasowe pole
 		private decimal _winStake;
+
+		private ImageSource _slot1;
+		private ImageSource _slot2;
+		private ImageSource _slot3;
 
 		public decimal BetStake
 		{
@@ -40,6 +46,36 @@ namespace WPFUI.ViewModels
 			{
 				_betInfo = value;
 				NotifyOfPropertyChange(() => BetInfo);
+			}
+		}
+
+		public ImageSource Slot1 
+		{ 
+			get { return _slot1; }
+			set 
+			{
+				_slot1 = value;
+				NotifyOfPropertyChange(() => Slot1);
+			}
+		}
+
+		public ImageSource Slot2
+		{
+			get { return _slot2; }
+			set
+			{
+				_slot2 = value;
+				NotifyOfPropertyChange(() => Slot2);
+			}
+		}
+
+		public ImageSource Slot3
+		{
+			get { return _slot3; }
+			set
+			{
+				_slot3 = value;
+				NotifyOfPropertyChange(() => Slot3);
 			}
 		}
 
@@ -68,11 +104,35 @@ namespace WPFUI.ViewModels
 			// tu dodac pobieranie kasy od usera, nawet zhardcodowanego i odpalic logike slotsow
 			slotsLogic.RandomChoose();
 			_winStake = slotsLogic.ComputeWin(BetStake);
+
 			foreach (var element in slotsLogic.RandomlyChosen)
 			{
 				Console.WriteLine(element);
 			}
+
 			Console.WriteLine($"{ _winStake }");
+
+			Slot1 = Images(slotsLogic.RandomlyChosen.ElementAt(0));
+			Slot2 = Images(slotsLogic.RandomlyChosen.ElementAt(1));
+			Slot3 = Images(slotsLogic.RandomlyChosen.ElementAt(2));
+
+			if (_winStake > 0)
+			{
+				MessageBox.Show($"Wygrałeś { _winStake }");
+			}
+		}
+
+		public ImageSource Images(char which) 
+		{
+			ImageSource art;
+			var converter = new ImageSourceConverter();
+			if (which == 'C') art = (ImageSource)converter.ConvertFromString("C:/Users/luke/source/repos/WpfApp1/WpfApp1/images/cherry.png");
+			else if (which == 'L') art = (ImageSource)converter.ConvertFromString("C:/Users/luke/source/repos/WpfApp1/WpfApp1/images/lemon.png");
+			else if (which == 'O') art = (ImageSource)converter.ConvertFromString("C:/Users/luke/source/repos/WpfApp1/WpfApp1/images/orange.png");
+			else if (which == 'P') art = (ImageSource)converter.ConvertFromString("C:/Users/luke/source/repos/WpfApp1/WpfApp1/images/plum.png");
+			else if (which == 'W') art = (ImageSource)converter.ConvertFromString("C:/Users/luke/source/repos/WpfApp1/WpfApp1/images/watermelon.png");
+			else art = (ImageSource)converter.ConvertFromString("C:/Users/luke/source/repos/WpfApp1/WpfApp1/slots.png");
+			return art;
 		}
 	}
 }
