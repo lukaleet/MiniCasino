254cb4c482ffa8b65bece9b0355d5728cf67ab20
luke <2lukaszmaron@gmail.com>
2020-06-06 22:38:20 +0200

    Added Few Functions To Blackjack Logic

--- Casino/Blackjack/BlackjackLogic.cs
+++ Casino/Blackjack/BlackjackLogic.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Casino.Blackjack
 {
@@ -8,8 +9,82 @@ namespace Casino.Blackjack
     {
         private Deck deck = new Deck();
 
+        public List<Card> playerHand { get; set; } = new List<Card>();
+
+        public List<Card> enemyHand { get; set; } = new List<Card>();
+
         public void Deal()
         {
+            playerHand.Add(deck.cards.ElementAt(0));
+
+            deck.cards.RemoveAt(0);
+
+            enemyHand.Add(deck.cards.ElementAt(0));
+
+            deck.cards.RemoveAt(0);
+
+            playerHand.Add(deck.cards.ElementAt(0));
+
+            deck.cards.RemoveAt(0);
+
+            enemyHand.Add(deck.cards.ElementAt(0));
+
+            deck.cards.RemoveAt(0);
         }
+
+        public void Hit()
+        {
+            playerHand.Add(deck.cards.ElementAt(0));
+
+            deck.cards.RemoveAt(0);
+        }
+
+        public bool Stay()
+        {
+            if (ShouldEnemyDrawCard(enemyHand))
+            {
+                enemyHand.Add(deck.cards.ElementAt(0));
+                deck.cards.RemoveAt(0);
+            }
+
+            bool win = ComputePlayerWin(playerHand, enemyHand);
+
+            return win;
+        }
+
+        public int HandValue(List<Card> cards)
+        {
+            int sum = 0;
+            foreach(var card in cards)
+            {
+                sum += card.GameValue;
+            }
+
+            return sum;
+        }
+
+        public bool ShouldEnemyDrawCard(List<Card> enemyCards)
+        {
+            if (HandValue(enemyCards) >= 17)
+                return false;
+            else
+                return true;
+        }
+
+        bool ComputePlayerWin(List<Card> playerCards, List<Card> enemyCards)
+        {
+            if ((HandValue(playerCards) > HandValue(enemyCards)) && (HandValue(playerCards) < 22))
+                return true;
+
+            else if ((HandValue(playerCards) == HandValue(enemyCards) && HandValue(playerCards) < 21))
+                return false;
+
+            else if ((HandValue(playerCards) < HandValue(enemyCards)) && HandValue(enemyCards) < 22)
+                return false;
+
+            else
+                return false;
+        }
+
     }
 }
